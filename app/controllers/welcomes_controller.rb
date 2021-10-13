c63d7a23c69bb1584f985e23f7022fcae93ecfa3
Kelly Kaufman <kellykaufman12@gmail.com>
2021-10-12 20:10:51 -0700

    Adds favorite language

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,2 +1,10 @@
+# app/controllers/welcomes_controller.rb
 class WelcomesController < ApplicationController
+  def hello_method
+    render json: { message: "Hello from welcomes controller!" }
+  end
+
+  def about_method
+    render json: { message: "My favorite language is now Ruby!" }
+  end
 end
